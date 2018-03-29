6602da2a0741bc2903ea3ac28d92395af495cffd
mounir <mounir.mahmoud@gmail.com>
2018-03-28 20:11:29 -0400

    add pluging fod docker images

---
+++ web-quote-project/web-quote-proxy-service/src/main/java/com/sunshine/quote/quoteproxyservice/events/QuoteGatewayController.java
@@ -0,0 +1,13 @@
+package com.sunshine.quote.quoteproxyservice.events;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fanida on 18-03-26.
+ */
+
+@RestController
+public class QuoteGatewayController {
+
+
+}
